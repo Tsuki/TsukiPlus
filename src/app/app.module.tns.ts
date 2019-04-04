1cd597df6064396fef45c2a19fcb926331915c83
Tsukatsuki Kanade <i@sukitsuki.com>
2019-04-04 21:47:28 +0700

    http client

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,16 +1,16 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import {NativeScriptModule} from 'nativescript-angular/nativescript.module';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 
 // Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
-// import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
+import {NativeScriptHttpClientModule} from 'nativescript-angular/http-client';
 
 @NgModule({
   declarations: [
@@ -20,9 +20,11 @@ import { HomeComponent } from './home/home.component';
   imports: [
     NativeScriptModule,
     AppRoutingModule,
+    NativeScriptHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+}
